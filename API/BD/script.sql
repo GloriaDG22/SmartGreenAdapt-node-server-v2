8ed39a97407d1fd7150c3ee2dc0672f5eb1abfa3
Maria Victoria Morales Millanes <ee24.gps.unex@gmail.com>
2022-03-28 23:52:00 +0200

    Cambios script y open api

--- API/BD/script.sql
+++ API/BD/script.sql
@@ -10,51 +10,43 @@ DROP TABLE if exists Irrigation;
 DROP TABLE if exists Sprinklers;
 DROP TABLE if exists Heating;
 CREATE TABLE AirQuality (
-	id INT NOT NULL AUTO_INCREMENT,
-    amount DECIMAL(4,2),
-    date TIMESTAMP,
-    min_value DECIMAL(4,2),
-    max_value DECIMAL(4,2),
-	PRIMARY KEY(id)
+id INT NOT NULL AUTO_INCREMENT,
+amount DECIMAL(4,2),
+date TIMESTAMP,
+PRIMARY KEY(id)
 );
 CREATE TABLE Luminosity (
-	id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     amount DECIMAL(4,2),
     date TIMESTAMP,
-    min_value DECIMAL(4,2),
-    max_value DECIMAL(4,2),
-    PRIMARY KEY(id)    
+    PRIMARY KEY(id)
 );
 CREATE TABLE Notification (
-	id INT NOT NULL AUTO_INCREMENT,
-    is_warning INT,
-    problem VARCHAR(100),
-    status VARCHAR(10),
-    date TIMESTAMP,
-    PRIMARY KEY(id)    
+  id INT NOT NULL AUTO_INCREMENT,
+  is_warning INT,
+  problem VARCHAR(100),
+  status VARCHAR(10),
+  date TIMESTAMP,
+  PRIMARY KEY(id)
 );
 CREATE TABLE Humidity (
-	id INT NOT NULL AUTO_INCREMENT,
-    amount DECIMAL(4,2),
-    date TIMESTAMP,
-    min_value DECIMAL(4,2),
-    max_value DECIMAL(4,2),
-    PRIMARY KEY(id)    
+  id INT NOT NULL AUTO_INCREMENT,
+  amount DECIMAL(4,2),
+  date TIMESTAMP,
+  PRIMARY KEY(id)
 );
 CREATE TABLE Temperature (
-	id INT NOT NULL AUTO_INCREMENT,
-    amount DECIMAL(4,2),  
-    date TIMESTAMP,
-    min_value DECIMAL(4,2),
-    max_value DECIMAL(4,2),
-    PRIMARY KEY(id)    
+ id INT NOT NULL AUTO_INCREMENT,
+ amount DECIMAL(4,2),
+ date TIMESTAMP,
+ PRIMARY KEY(id)
 );
 CREATE TABLE Windows (
-    id INT NOT NULL AUTO_INCREMENT,
-    is_on INT,
-    affects VARCHAR(100),
-    name VARCHAR(100),
-    PRIMARY KEY(id)
+     id INT NOT NULL AUTO_INCREMENT,
+     is_on INT,
+     affects VARCHAR(100),
+     name VARCHAR(100),
+     PRIMARY KEY(id)
 );
 CREATE TABLE Irrigation(
    id INT NOT NULL AUTO_INCREMENT,
@@ -69,23 +61,23 @@ CREATE TABLE Sprinklers(
    PRIMARY KEY(id)
 );
 CREATE TABLE Heating(
-     id INT NOT NULL AUTO_INCREMENT,
-     is_on INT,
-     affects VARCHAR(100),
-     type VARCHAR(50),
-     PRIMARY KEY(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    affects VARCHAR(100),
+    type VARCHAR(50),
+    value INT,
+    PRIMARY KEY(id)
 );
 CREATE TABLE Weather (
-	id INT NOT NULL AUTO_INCREMENT,
-    state VARCHAR(10),  
-    temp DECIMAL(4,2),
-    temp_feel DECIMAL(4,2),
-    temp_min DECIMAL(4,2),
-    temp_max DECIMAL(4,2),
-    pressure INT,
-    humidity INT,
-    wind DECIMAL(5,2),
-    date TIMESTAMP,
-    season VARCHAR(10),
-    PRIMARY KEY(id)    
+ id INT NOT NULL AUTO_INCREMENT,
+ state VARCHAR(10),
+ temp DECIMAL(4,2),
+ temp_feel DECIMAL(4,2),
+ temp_min DECIMAL(4,2),
+ temp_max DECIMAL(4,2),
+ pressure INT,
+ humidity INT,
+ wind DECIMAL(5,2),
+ date TIMESTAMP,
+ season VARCHAR(10),
+ PRIMARY KEY(id)
 );
