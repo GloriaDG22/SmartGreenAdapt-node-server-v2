a6bc51d3ebbb47d83a99e339d055a87074e66dac
Maria Victoria Morales Millanes <ee24.gps.unex@gmail.com>
2022-03-29 21:34:41 +0200

    Nuevas tablas de usuarios e invernadero

--- API/BD/script.sql
+++ API/BD/script.sql
@@ -10,20 +10,40 @@ DROP TABLE if exists Windows;
 DROP TABLE if exists Irrigation;
 DROP TABLE if exists Sprinklers;
 DROP TABLE if exists Heating;
+DROP TABLE if exists GreenHouse;
+DROP TABLE if exists UserInfo;
 DROP TABLE if exists weather;
 
+CREATE TABLE UserInfo(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(25),
+    password VARCHAR(25),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE GreenHouse(
+  id INT NOT NULL AUTO_INCREMENT,
+  idUsuario INT,
+  name VARCHAR(25),
+  PRIMARY KEY (id),
+  FOREIGN KEY (idUsuario) REFERENCES UserInfo(id)
+);
 
 CREATE TABLE AirQuality (
 	id INT NOT NULL AUTO_INCREMENT,
     amount DECIMAL(4,2),
     date TIMESTAMP,
-	PRIMARY KEY(id)
+    idGreenhouse INT,
+	PRIMARY KEY(id),
+    FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Luminosity (
     id INT NOT NULL AUTO_INCREMENT,
     amount DECIMAL(6,2),
     date TIMESTAMP,
-    PRIMARY KEY(id)
+    idGreenhouse INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Notification (
     id INT NOT NULL AUTO_INCREMENT,
@@ -31,45 +51,59 @@ CREATE TABLE Notification (
     problem VARCHAR(100),
     status VARCHAR(10),
     date TIMESTAMP,
-    PRIMARY KEY(id)
+    idGreenhouse INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Humidity (
     id INT NOT NULL AUTO_INCREMENT,
     amount DECIMAL(4,2),
     date TIMESTAMP,
-    PRIMARY KEY(id)
+    idGreenhouse INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Temperature (
 	id INT NOT NULL AUTO_INCREMENT,
     amount DECIMAL(4,2),  
     date TIMESTAMP,
-    PRIMARY KEY(id)    
+    idGreenhouse INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Windows (
     id INT NOT NULL AUTO_INCREMENT,
     is_on INT,
     affects VARCHAR(100),
     name VARCHAR(100),
-    PRIMARY KEY(id)
+    idGreenhouse INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Irrigation(
    id INT NOT NULL AUTO_INCREMENT,
    is_on INT,
    affects VARCHAR(100),
-   PRIMARY KEY(id)
+   idGreenhouse INT,
+   PRIMARY KEY(id),
+   FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Sprinklers(
    id INT NOT NULL AUTO_INCREMENT,
    is_on INT,
    affects VARCHAR(100),
-   PRIMARY KEY(id)
+   idGreenhouse INT,
+   PRIMARY KEY(id),
+   FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Heating(
     id INT NOT NULL AUTO_INCREMENT,
     affects VARCHAR(100),
     type VARCHAR(50),
     value INT,
-    PRIMARY KEY(id)
+    idGreenhouse INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (idGreenhouse) REFERENCES GreenHouse(id)
 );
 CREATE TABLE Weather (
 	id INT NOT NULL AUTO_INCREMENT,
@@ -86,45 +120,53 @@ CREATE TABLE Weather (
     PRIMARY KEY(id)    
 );
 
+-- Creación de usuario e invernadero de prueba
+
+INSERT INTO smartgreenadapt.UserInfo(username, password)
+VALUES ('user', 'user');
+
+INSERT INTO smartgreenadapt.GreenHouse(idUsuario, name)
+VALUES (1, 'Invernadero 1');
+
 -- Prueba 1 - Warning humidity y error luminosity
-INSERT INTO smartgreenadapt.temperature (amount, date)
-VALUES (30.00, '2022-03-22 12:10:44');
-INSERT INTO smartgreenadapt.luminosity (amount, date)
-VALUES (1600.00, '2022-03-22 12:10:44');
-INSERT INTO smartgreenadapt.airquality (amount, date)
-VALUES (10.00, '2022-03-28 12:10:44');
-INSERT INTO smartgreenadapt.humidity (amount, date)
-VALUES (40.00, '2022-03-22 12:10:44');
+INSERT INTO smartgreenadapt.temperature (amount, date, idGreenhouse)
+VALUES (30.00, '2022-03-22 12:10:44', 1);
+INSERT INTO smartgreenadapt.luminosity (amount, date, idGreenhouse)
+VALUES (1600.00, '2022-03-22 12:10:44', 1);
+INSERT INTO smartgreenadapt.airquality (amount, date, idGreenhouse)
+VALUES (10.00, '2022-03-28 12:10:44', 1);
+INSERT INTO smartgreenadapt.humidity (amount, date, idGreenhouse)
+VALUES (40.00, '2022-03-22 12:10:44', 1);
 
 -- Prueba 2 - Error luminosity y warning airquality
-INSERT INTO smartgreenadapt.temperature (amount, date)
-VALUES (20.00, '2022-03-23 12:10:44');
-INSERT INTO smartgreenadapt.luminosity (amount, date)
-VALUES (-10.00, '2022-03-23 12:10:44');
-INSERT INTO smartgreenadapt.airquality (amount, date)
-VALUES (60.00, '2022-03-23 12:10:44');
-INSERT INTO smartgreenadapt.humidity (amount, date)
-VALUES (70.00, '2022-03-23 12:10:44');
+INSERT INTO smartgreenadapt.temperature (amount, date, idGreenhouse)
+VALUES (20.00, '2022-03-23 12:10:44', 1);
+INSERT INTO smartgreenadapt.luminosity (amount, date, idGreenhouse)
+VALUES (-10.00, '2022-03-23 12:10:44', 1);
+INSERT INTO smartgreenadapt.airquality (amount, date, idGreenhouse)
+VALUES (60.00, '2022-03-23 12:10:44', 1);
+INSERT INTO smartgreenadapt.humidity (amount, date, idGreenhouse)
+VALUES (70.00, '2022-03-23 12:10:44', 1);
 
 -- Prueba 3 - Warning luminosity y error temperature
-INSERT INTO smartgreenadapt.temperature (amount, date)
-VALUES (50.00, '2022-03-24 12:10:44');
-INSERT INTO smartgreenadapt.luminosity (amount, date)
-VALUES (1200.00, '2022-03-24 12:10:44');
-INSERT INTO smartgreenadapt.airquality (amount, date)
-VALUES (30.00, '2022-03-24 12:10:44');
-INSERT INTO smartgreenadapt.humidity (amount, date)
-VALUES (70.00, '2022-03-24 12:10:44');
+INSERT INTO smartgreenadapt.temperature (amount, date, idGreenhouse)
+VALUES (50.00, '2022-03-24 12:10:44', 1);
+INSERT INTO smartgreenadapt.luminosity (amount, date, idGreenhouse)
+VALUES (1200.00, '2022-03-24 12:10:44', 1);
+INSERT INTO smartgreenadapt.airquality (amount, date, idGreenhouse)
+VALUES (30.00, '2022-03-24 12:10:44', 1);
+INSERT INTO smartgreenadapt.humidity (amount, date, idGreenhouse)
+VALUES (70.00, '2022-03-24 12:10:44', 1);
 
 
 
 
 
-INSERT INTO smartgreenadapt.notification (is_warning, problem, status, date)
-VALUES (0, 'Temperature', 'low', '2022-03-22 12:10:44');
-INSERT INTO smartgreenadapt.notification (is_warning, problem, status, date)
-VALUES (0, 'Humidity', 'high', '2022-03-23 12:10:44');
-INSERT INTO smartgreenadapt.notification (is_warning, problem, status, date)
-VALUES (1, 'Temperature', 'error', '2022-03-24 12:10:44');
+INSERT INTO smartgreenadapt.notification (is_warning, problem, status, date, idGreenhouse)
+VALUES (0, 'Temperature', 'low', '2022-03-22 12:10:44', 1);
+INSERT INTO smartgreenadapt.notification (is_warning, problem, status, date, idGreenhouse)
+VALUES (0, 'Humidity', 'high', '2022-03-23 12:10:44', 1);
+INSERT INTO smartgreenadapt.notification (is_warning, problem, status, date, idGreenhouse)
+VALUES (1, 'Temperature', 'error', '2022-03-24 12:10:44', 1);
 
 
